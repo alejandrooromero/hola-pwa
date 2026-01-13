e82251400d4bb0edfd6047ce734765b72be29b44
alejandrooromero <59692539+alejandrooromero@users.noreply.github.com>
2026-01-12 22:30:02 -0300

    Add files via upload
    correcion en version android

--- sw.js
+++ sw.js
@@ -1,21 +1,37 @@
-const CACHE_NAME = 'hola-pwa-v1';
+const CACHE_NAME = 'hola-pwa-v2';
 const URLS_A_CACHEAR = [
   './',
-  './index.html'
+  './index.html',
+  './app.js',
+  './manifest.json'
 ];
 
+// Instalar y cachear
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(URLS_A_CACHEAR);
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_A_CACHEAR))
+  );
+  self.skipWaiting();
+});
+
+// Activar y borrar caché vieja
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
     })
   );
 });
 
+// Fetch
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(respuesta => {
-      return respuesta || fetch(event.request);
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
     })
   );
 });
